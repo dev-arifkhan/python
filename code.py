b8adc6cb4a6649b0ad455dd7d186991205e6c464
Arif <arifkhan250905@gmail.com>
2025-12-13 19:42:55 +0530

    creat a python file

---
+++ code.py
@@ -0,0 +1,12 @@
+# code to check vowel
+count = 0
+a = input("Enter a text: ").lower()
+for vow in a:
+    if vow == "a" or vow == "e" or vow == "i" or vow == "o" or vow == "u":
+         count += 1
+
+print(f"{a} has {count} vowel")        
+     
+
+
+
