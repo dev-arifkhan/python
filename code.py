1ed2e9602371059a78d2023ee8ac5605f508a5c4
Arif Khan <arifkhan250905@gmail.com>
2025-12-14 10:30:14 +0530

    new changes

--- code.py
+++ code.py
@@ -5,8 +5,4 @@ for vow in a:
     if vow == "a" or vow == "e" or vow == "i" or vow == "o" or vow == "u":
          count += 1
 
-print(f"{a} has {count} vowel")        
-     
-
-
-
+print(f"{a} has {count} vowel")  
