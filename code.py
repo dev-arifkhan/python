0460fad40c0bb278f104c4d1bdf088ae472c6353
dev-arifkhan <arifkhan250905@gmail.com>
2025-12-15 23:40:56 +0530

    add avg ,merg and sort code

--- code.py
+++ code.py
@@ -16,4 +16,27 @@ for num in numbers :
         print(f"the number at indx no {indx}")
         break
     indx += 1
-                
+# check palindrome
+n = "aia"
+rev_n =""
+for pal in n:
+    rev_n = pal+rev_n
+print(rev_n)
+if rev_n == n:
+    print(f"{n} was a palindrome")
+
+# average of list numbers
+num = [10,20,30,40]
+total = 0
+for n in num:
+    total = total + n
+avg = total / len(num)
+print(int(avg))
+
+# Merg & Short list 
+list1 = [1,2,3,10,4,5]
+list2 = [5,6,7,8,9]
+list3 = list1 + list2
+list3.sort()
+print(list3)
+
