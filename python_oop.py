bdc4ccda412ae9501487e9a20e50bbf462d7a7f3
dev-arifkhan <arifkhan250905@gmail.com>
2026-03-08 23:58:58 +0530

    nothing

--- python_oop.py
+++ python_oop.py
@@ -19,17 +19,59 @@ print(my_tesla.full_name())
 
 
 
+my_car = Car("Toyota","Corolla")
+print(my_car.brand)
+print(my_car.full_name())
+
+class Bank:
+    def __init__(self,balance,withdraw):
+        self.balance = balance
+        self.withdraw = withdraw
+    def total_balance (self):
+        return (self.balance-self.withdraw)
+    
 
 
 
+Customer1 =Bank(500,200)
+Customer2 = Bank("1000","5000")
+print(f"After withdrawal ₹{Customer1.withdraw} now account balance ₹{Customer1.total_balance()}")
 
+class Store:
+    def __init__(self,phone,price):
+        self.phone=phone
+        self.price=price
+    def full_details(self):
+        return f"{self.phone} price ₹{self.price}" 
+phone1=Store("iphone",1000)
+phone2=Store("Samsung",30000)
+print(phone1.price)
+print(phone2.full_details())
 
+class Student:
+    college_name = "ABC college"
 
+    def __init__(self,name,cgpa):
+        self.name= name
+        self.cgpa=cgpa
 
+stu1 = Student("Arif",9.5)        
+print(Student.college_name)
+print(stu1.college_name)
+# instance , class & static
+class Laptop:
+    storage_type = "SSD"
+    def __init__(self,RAM,storage):
+        self.RAM = RAM
+        self.storage = storage
+    @classmethod    
+    def get_storage_info(cls):
+        print(f"the storage type was {cls.storage_type}")   
 
-
-
-
-# my_car = Car("Toyota","Corolla")
-# print(my_car.brand)
-# print(my_car.full_name())
+    def get_info(self):
+        print(f"laptop has {self.RAM} RAM & {self.storage} {self.storage_type}")
+        
+l1 = Laptop("8gb","1TB")
+l2 = Laptop("4gb","500gb")
+l1.get_storage_info()
+l2.get_info()
